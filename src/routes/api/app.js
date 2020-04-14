eef33621ea794ece3fc2346cffc310ede044e5e7
Kenneth Chirchir <kipkogeichirchir2@gmail.com>
2020-04-14 19:33:42 +0000

    t

--- src/routes/api/app.js
+++ src/routes/api/app.js
@@ -7,29 +7,29 @@ const estimator = require('../../estimator');
 // const xml = require('xml')
 // import estimator from '../../estimator'
 router.post('/on-covid-19', (req, res) => {
-  const { data } = req.body;
-  // console.log(req.body);
-  const output = estimator(data);
-  return res.status(200).json({ output });
+    const { data } = req.body;
+    // console.log(req.body);
+    const output = estimator(data);
+    return res.status(200).json({ output });
 });
 router.post('/on-covid-19/json', (req, res) => {
-  const { data } = req.body;
-  // console.log(req.body);
-  const output = estimator(data);
-  return res.status(200).json({ output });
+    const { data } = req.body;
+    //console.log(data);
+    const output = estimator(data);
+    return res.status(200).json({ output });
 });
 router.post('/on-covid-19/xml', async (req, res) => {
-  const { root } = req.body;
+    const { root } = req.body;
 
-  let output = estimator(root);
-  output = await js2xmlparser.parse('root', output);
-  return res.set('Content-Type', 'text/xml').status(200).send(output);
+    let output = estimator(root);
+    output = await js2xmlparser.parse('root', output);
+    return res.set('Content-Type', 'text/xml').status(200).send(output);
 });
 
 router.get('/on-covid-19/logs', async (req, res) => {
-  const log = fs.readFileSync(path.join(__dirname, '../../logs.txt'));
-  return res.type('text/plain')
-    .status(200).send(log);
+    const log = fs.readFileSync(path.join(__dirname, '../../logs.txt'));
+    return res.type('text/plain')
+        .status(200).send(log);
 });
 
 
